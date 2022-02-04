423aedb3af862b9a312c1deb25938b2c1cca54a3
Raphael velasco <raphaelvelasco2@gmail.com>
2022-02-04 11:18:14 -0300

    Fiz o desafio

---
+++ script.js
@@ -0,0 +1,10 @@
+let frutas = ['Maçã', 'Banana', 'Laranja', 'Tangerina', 'Limão'];
+let add1 = frutas.unshift('Morango');
+let ultimo = frutas.pop();
+let add2 = frutas.push('Kiwi', 'Manga');
+let removedItem = frutas.splice('1', '1');
+console.log(frutas)
+
+const numbers = [7,5,6,3,8,9,2,1,4];
+numbers.sort()
+console.log(numbers)
